4d33554667b58d9ea6acadb187a2bdf974e2d1d9
Nishant Tanaji Shedage <nishantt@geekyants.com>
2023-10-08 14:04:25 +0530

    initial commit

---
+++ components/Layout/Header.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { ModeToggle } from "./components/ModeToggle";
+
+function Header() {
+  return (
+    <div>
+      <ModeToggle />
+    </div>
+  );
+}
+
+export default Header;
