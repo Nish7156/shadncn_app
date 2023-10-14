736f0109f3ec30b72af330ae1b6ac164341a70da
Your Name <your.email@example.com>
2023-10-14 23:25:57 +0530

    seo setup

---
+++ lib/config.ts
@@ -0,0 +1,7 @@
+export const siteConfig = {
+    title: "Nishant Shedage || Developer",
+    description:
+      "A web developer is a professional responsible for creating and maintaining websites and web applications. They work on the visual elements (front-end) and/or the behind-the-scenes functionality (back-end) to ensure websites are user-friendly, responsive, and perform well across various devices and browsers. Web developers may specialize in front-end, back-end, or full-stack development, using languages and technologies like HTML, CSS, JavaScript, PHP, Python, and databases to bring web projects to life. They play a crucial role in building and enhancing an organization's online presence.",
+    url: "webtriggers.tech",
+  };
+  
