32ea7c47a933fac9cd7bf138ed3f831d01367334
Your Name <your.email@example.com>
2023-11-05 17:02:02 +0530

    navbar

---
+++ components/Layout/components/Navbar.tsx
@@ -0,0 +1,150 @@
+"use client";
+import React, { useState } from "react";
+import { ModeToggle } from "./ModeToggle";
+
+const Navbar = () => {
+  const [isMobileMenuOpen, setMobileMenuOpen] = useState(false);
+
+  const toggleMobileMenu = () => {
+    setMobileMenuOpen(!isMobileMenuOpen);
+  };
+
+  const menuItems = [
+    { text: "Dashboard", href: "#" },
+    { text: "Team", href: "#" },
+    { text: "Projects", href: "#" },
+    { text: "Calendar", href: "#" },
+  ];
+
+  return (
+    <nav className="bg-gray-800">
+      <div className="mx-auto max-w-7xl px-2 sm:px-6 lg:px-8">
+        <div className="relative flex h-16 items-center justify-between">
+          {/* Mobile menu button */}
+          <div className="absolute inset-y-0 left-0 flex items-center sm:hidden">
+            <button
+              type="button"
+              onClick={toggleMobileMenu}
+              className="relative inline-flex items-center justify-center rounded-md p-2 text-gray-400 hover:bg-gray-700 hover:text-white focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white"
+              aria-controls="mobile-menu"
+              aria-expanded={isMobileMenuOpen}
+            >
+              {/* Icon when menu is closed */}
+              <svg
+                className={`h-6 w-6 ${isMobileMenuOpen ? "hidden" : "block"}`}
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke-width="1.5"
+                stroke="currentColor"
+                aria-hidden="true"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5"
+                />
+              </svg>
+              {/* Icon when menu is open */}
+              <svg
+                className={`h-6 w-6 ${isMobileMenuOpen ? "block" : "hidden"}`}
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke-width="1.5"
+                stroke="currentColor"
+                aria-hidden="true"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  d="M6 18L18 6M6 6l12 12"
+                />
+              </svg>
+            </button>
+          </div>
+
+          <div className="flex flex-1 items-center justify-center sm:items-stretch sm:justify-start">
+            <div className="flex flex-shrink-0 items-center">
+              <img
+                className="h-8 w-auto"
+                src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=500"
+                alt="Your Company"
+              />
+            </div>
+            <div className="hidden sm:ml-6 sm:block">
+              <div className="flex space-x-4">
+                {menuItems.map((item) => (
+                  <a
+                    key={item.text}
+                    href={item.href}
+                    className={`${
+                      item.text === "Dashboard"
+                        ? "bg-gray-900 text-white"
+                        : "text-gray-300 hover:bg-gray-700 hover:text-white"
+                    } rounded-md px-3 py-2 text-sm font-medium`}
+                    aria-current={
+                      item.text === "Dashboard" ? "page" : undefined
+                    }
+                  >
+                    {item.text}
+                  </a>
+                ))}
+              </div>
+            </div>
+          </div>
+
+          <div className="absolute inset-y-0 right-0 flex items-center pr-2 sm:static sm:inset-auto sm:ml-6 sm:pr-0">
+            {/* Add notification button here */}
+
+            {/* Profile dropdown */}
+            <div className="relative ml-3">
+              <div>
+                <button
+                  type="button"
+                  className="relative flex rounded-full bg-gray-800 text-sm focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800"
+                  id="user-menu-button"
+                  aria-expanded="false"
+                  aria-haspopup="true"
+                >
+                  <span className="absolute -inset-1.5"></span>
+                  <span className="sr-only">Open user menu</span>
+                  <img
+                    className="h-8 w-8 rounded-full"
+                    src="https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"
+                    alt=""
+                  />
+                </button>
+              </div>
+            </div>
+            <div className="ml-4">
+              <ModeToggle />
+            </div>
+          </div>
+        </div>
+      </div>
+
+      {/* Mobile menu */}
+      {isMobileMenuOpen && (
+        <div className="sm:hidden" id="mobile-menu">
+          <div className="space-y-1 px-2 pb-3 pt-2">
+            {menuItems.map((item) => (
+              <a
+                key={item.text}
+                href={item.href}
+                className={`${
+                  item.text === "Dashboard"
+                    ? "bg-gray-900 text-white"
+                    : "text-gray-300 hover:bg-gray-700 hover:text-white"
+                } block rounded-md px-3 py-2 text-base font-medium`}
+                aria-current={item.text === "Dashboard" ? "page" : undefined}
+              >
+                {item.text}
+              </a>
+            ))}
+          </div>
+        </div>
+      )}
+    </nav>
+  );
+};
+
+export default Navbar;
--- lib/config.ts
+++ lib/config.ts
@@ -1,7 +1,6 @@
 export const siteConfig = {
-    title: "Nishant Shedage || Developer",
-    description:
-      "A web developer is a professional responsible for creating and maintaining websites and web applications. They work on the visual elements (front-end) and/or the behind-the-scenes functionality (back-end) to ensure websites are user-friendly, responsive, and perform well across various devices and browsers. Web developers may specialize in front-end, back-end, or full-stack development, using languages and technologies like HTML, CSS, JavaScript, PHP, Python, and databases to bring web projects to life. They play a crucial role in building and enhancing an organization's online presence.",
-    url: "webtriggers.tech",
-  };
-  
+  title: "Premium Car Wash Services for Your Vehicle",
+  description:
+    "Experience top-tier car cleaning with Car Wash. Our services include expert exterior wash, interior detailing, and eco-friendly practices. Book your appointment for a sparkling car today!",
+  url: "webtriggers.tech",
+};
